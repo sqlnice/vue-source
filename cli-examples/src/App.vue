b4ee99ac8e72e2d190d6ad04b762cbedb90e4ef6
时启龙 <shiqilong@beyondcent.com>
2022-03-04 16:38:38 +0800

    feat: 13.组件化-异步组件

--- cli-examples/src/App.vue
+++ cli-examples/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <async-webpack-example></async-webpack-example>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
