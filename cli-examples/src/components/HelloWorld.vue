745b8b9180c86983be7577eda1c48f6c9b7274a4
时启龙 <shiqilong@beyondcent.com>
2022-03-21 17:59:11 +0800

    fix: update the Vue version in example

--- cli-examples/src/components/HelloWorld.vue
+++ cli-examples/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ nested.msg }}</h1>
+    <!-- <h1>{{ fullName }}</h1> -->
   </div>
 </template>
 
@@ -8,35 +8,15 @@
 export default {
   name: "HelloWorld",
   data() {
-    return {
-      nested: {
-        msg: "Hello Message",
-      },
-    };
+    return {};
   },
-  mounted() {
-    console.log(1);
-    setTimeout(() => {
-      this.nested.msg = "修改后";
-    }, 1000);
+  computed: {
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    },
   },
+  mounted() {},
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
