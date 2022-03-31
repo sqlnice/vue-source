6a8882678b3b4ba37b9424584b5953161f94bc06
sqlnice <472252510@qq.com>
2022-03-31 23:27:57 +0800

    docs: 20.编译-编译入口 增加代码注释

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -12,10 +12,13 @@ export const createCompiler = createCompilerCreator(function baseCompile (
   template: string,
   options: CompilerOptions
 ): CompiledResult {
+  // 1. 解析模板字符串生成 AST
   const ast = parse(template.trim(), options)
   if (options.optimize !== false) {
+    // 2. 优化语法树
     optimize(ast, options)
   }
+  // 3. 生成代码
   const code = generate(ast, options)
   return {
     ast,
